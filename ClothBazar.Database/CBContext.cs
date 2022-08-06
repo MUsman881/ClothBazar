1f57750353343d4177771462750de47352844341
mohamadusman881 <mohamadusman881@gmail.com>
2022-08-07 00:59:18 +0500

    Added Entity Framework and Created  Database

--- ClothBazar.Database/CBContext.cs
+++ ClothBazar.Database/CBContext.cs
@@ -10,7 +10,7 @@ namespace ClothBazar.Database
 {
     public class CBContext : DbContext  
     {
-        public CBContext() : base ("name=ClothBazarConnection")
+        public CBContext() : base ("ClothBazarConnection")
         {
 
         }
