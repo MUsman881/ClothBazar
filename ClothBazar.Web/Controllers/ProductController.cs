752feb366ae0c35ef604b3ef830fdf7f4e81368a
mohamadusman881 <mohamadusman881@gmail.com>
2022-08-07 17:41:35 +0500

    Basic CRUD operations on Category and Product

---
+++ ClothBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ClothBazar.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClothBazar.Entities;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductTable(string search)
+        {
+            var products = productService.GetProducts();
+
+            if(string.IsNullOrEmpty(search) == false)
+            {
+                products = products.Where(p => p.Name != null && p.Name.ToLower().Contains(search.ToLower())).ToList();
+
+            }
+
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productService.SaveProduct(product);
+
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
